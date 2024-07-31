03ee431fe69b6cdfd6539e6b5ce8b4976e45bcb5
fangzheng <fangzheng@aimstek.cn>
2024-07-31 17:15:10 +0800

    first-commit

---
+++ src/main/java/com/fg/comment/FgCommentApplication.java
@@ -0,0 +1,13 @@
+package com.fg.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FgCommentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( FgCommentApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/fg/comment/FgCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fg.comment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FgCommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
